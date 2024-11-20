9ce1bd437073ca5c1d6d5c1c16acd7afda494360
pabnie1234 <pablo.nieva@educatec.net>
2024-11-20 22:34:30 +0100

    Update calculadora.java

---
+++ calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+
+    private static int suma(int a, int b) {
+        return 0; // TODO
+    }
+
+    private static int resta(int a, int b) {
+        return 0; // TODO
+    }
+
+    private static int multiplicacion(int a, int b) {
+        return 0; // TODO
+    }
+
+    private static double division(int a, int b) {
+        return 0; // TODO
+    }
+
+    private static int modulo(int a, int b) {
+        return 0; // TODO
+    }
+}
